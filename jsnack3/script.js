de640c06129984e724b3e84893cb08c400496936
lucadisanto7 <lucadisanto47@gmail.com>
2024-07-01 15:25:40 +0200

    [UPD] Added if in js

--- jsnack3/script.js
+++ jsnack3/script.js
@@ -1,3 +1,10 @@
 // Chiedi un numero di 4 cifre all'utente
 let number = prompt("Inserisci un numero di 4 cifre:");
 
+// Verifica che il numero sia di 4 cifre
+if (number.length !== 4) {
+    console.log("Per favore, inserisci un numero valido di 4 cifre.");
+} else {
+    // Dividi il numero in singole cifre
+    let digits = number.split('');
+}
