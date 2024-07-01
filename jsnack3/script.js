2e88c9d30c9aca4deb4e57327b4b4d2a7ea95cee
lucadisanto7 <lucadisanto47@gmail.com>
2024-07-01 15:26:57 +0200

    [UPD] Added FOR in if

--- jsnack3/script.js
+++ jsnack3/script.js
@@ -7,4 +7,10 @@ if (number.length !== 4) {
 } else {
     // Dividi il numero in singole cifre
     let digits = number.split('');
+    // Calcola la somma delle cifre
+    let sum = 0;
+    for (let i = 0; i < digits.length; i++) {
+        sum += parseInt(digits[i]);
+    }
+
 }
