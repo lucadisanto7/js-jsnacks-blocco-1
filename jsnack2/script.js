9a8ebac3c9274a2ba6053deca4835091cec2e2f4
lucadisanto7 <lucadisanto47@gmail.com>
2024-07-01 15:01:18 +0200

    [UPD] Added index.html and js of second snack

---
+++ jsnack2/script.js
@@ -0,0 +1,15 @@
+// Crea un array vuoto
+let oddNumbers = [];
+
+// Chiedi per 6 volte all'utente di inserire un numero
+for (let i = 0; i < 6; i++) {
+    let number = prompt("Inserisci un numero:");
+
+    // Verifica se il numero è dispari
+    if (number % 2 !== 0) {
+        // Inserisci il numero dispari nell'array
+        oddNumbers.push(number);
+    }
+}
+
+console.log("Numeri dispari inseriti:", oddNumbers);
